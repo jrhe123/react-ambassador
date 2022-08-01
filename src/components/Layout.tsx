ca0094b5640f47612df16c3c5ef818ced0247525
jrhe1213 <jiarong1213@gmail.com>
2022-07-31 23:17:46 -0400

    layout fetch user info

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,35 @@
-import React, { FC } from "react";
+import React, { Dispatch, FC, useState, useEffect } from "react";
+import { User } from "../models/user";
 import Header from "./Header";
+import { Navigate } from "react-router-dom";
 import Nav from "./Nav";
+import { connect } from "react-redux";
+import { setUser } from "../redux/actions/setUserAction";
+import axios from "axios";
 
 interface LayoutProps {
   children: React.ReactNode;
+  setUserDispatch: (user: User) => void;
 }
 
-const Layout: FC<LayoutProps> = ({ children }) => {
+const Layout: FC<LayoutProps> = ({ children, setUserDispatch }) => {
+  const [redirect, setRedirect] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("user");
+        setUserDispatch(response.data);
+      } catch (error) {
+        setRedirect(true);
+      }
+    })();
+  }, []);
+
+  if (redirect) {
+    return <Navigate to="/login" replace />;
+  }
+
   return (
     <div>
       <Nav />
@@ -20,4 +43,12 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   );
 };
 
-export default Layout;
+const mapStateToProps = (state: { user: User }) => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+  setUserDispatch: (user: User) => dispatch(setUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
